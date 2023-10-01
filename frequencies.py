3cc4efb42e5522e5ba15c15de0119d5246130eb9
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-01 23:17:54 +0100

    Complete frequencies.py to correctly return frequencies in a dictionary

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    str_list = [str(item) for item in items]    # turns all items to strings
+    for item in str_list:
+        count = 1
+        if item in frequencies:                 # for any duplicate items
+            count = frequencies.get(item) + 1
+        frequencies.update({item: count})
     return frequencies
